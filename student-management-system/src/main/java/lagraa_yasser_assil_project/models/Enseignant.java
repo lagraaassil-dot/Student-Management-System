11158ceae3ce8eb31ee2b681c1a746839a73d820
Lagraa Assil <lagraaassil@gmail.com>
2026-04-22 10:43:34 +0100

    finished models.2

--- student-management-system/src/main/java/lagraa_yasser_assil_project/models/Enseignant.java
+++ student-management-system/src/main/java/lagraa_yasser_assil_project/models/Enseignant.java
@@ -2,18 +2,15 @@ package lagraa_yasser_assil_project.models;
 import lagraa_yasser_assil_project.Enums.*;
 
 public class Enseignant {
-
-private int final idEnseignant;
+private int idEnseignant;
 private String nom;
 private Specialite specialite;
 
-
-public Enseignant( String nom, Specialite specialite) {
-
+// Constructor
+public Enseignant(int idEnseignant, String nom, Specialite specialite) {
+    this.idEnseignant = idEnseignant; //PK so no setter for this field 
     this.nom = nom;
-    this.specialite = specialite;
-    //function to generate ID
-}
+    this.specialite = specialite;}
 
 // Getters
 public int getIdEnseignant() {return idEnseignant; }   
